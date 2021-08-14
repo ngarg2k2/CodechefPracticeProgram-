c6ed32ae0878bd42ffaeccb2e1c95c5877de6829
Nipun Garg <garg2002nipun@gmail.com>
2021-08-14 11:33:42 +0530

    8

---
+++ SmallFactorials.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * fact(n - 1);
+    }
+}
+int main()
+{
+
+    {
+        int t;
+        cin >> t;
+        while (t--)
+        {
+            int n;
+            cin >> n;
+            cout << fact(n) << endl;
+        }
+        return 0;
+    }
+}
