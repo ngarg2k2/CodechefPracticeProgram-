df4833562dfb8b16ef4aad62ef5b708b3a19b478
Nipun Garg <garg2002nipun@gmail.com>
2021-08-14 13:42:56 +0530

    12

---
+++ FindingSquareRoots.cpp
@@ -0,0 +1,22 @@
+//12
+#include<iostream>
+using namespace std;
+void sqroot(){
+    int n,p=0;
+    cin>>n;
+    for (int i = 0; i=(p*p<=n) ; i++)
+    {
+        p++;
+    }
+    cout<<p-1<<endl;
+    
+}
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        sqroot();
+    }
+    
+}
